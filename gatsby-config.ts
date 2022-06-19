2fb064c83265278cf61f94199e242cbac263f796
MohammadArik <mohammadarik63@gmail.com>
2022-06-19 10:56:44 +0600

    Tailwind added

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,11 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `pomodorox`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-postcss"],
+};
 
-export default config
+export default config;
