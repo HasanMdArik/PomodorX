6088e44f2a7208bf434163d9489ced6d2622d2c8
Mohammad <mohammadarik63@gmail.com>
2022-07-06 11:02:19 +0600

    runningStep related fields added

--- src/data/dataTypes.ts
+++ src/data/dataTypes.ts
@@ -6,6 +6,8 @@ type timeStepsData = {
 };
 
 interface mainContextInterface {
+  runningStep: number;
+  setRunningStep: React.Dispatch<React.SetStateAction<number>>;
   stepsData: timeStepsData;
   setStepsData: React.Dispatch<React.SetStateAction<timeStepsData>>;
 }
