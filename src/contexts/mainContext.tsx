4ace7a4442b95d5fa619bcf0ad700b6c78c4e111
Mohammad <mohammadarik63@gmail.com>
2022-07-06 07:00:08 +0600

    main context primarily done

---
+++ src/contexts/mainContext.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, useContext, useState } from "react";
+import { mainContextInterface, timeStepsData } from "../data/dataTypes";
+
+const MainContext = React.createContext({} as mainContextInterface);
+
+export const useMainContext = () => {
+  return useContext(MainContext);
+};
+
+const MainContextProvider = ({ children }: { children: ReactNode }) => {
+  const [stepsData, setStepsData] = useState<timeStepsData>({
+    pastStepsCount: 0,
+    stepsArray: [],
+  });
+
+  const contextValues: mainContextInterface = {
+    stepsData,
+    setStepsData,
+  };
+
+  return (
+    <MainContext.Provider value={contextValues}>
+      {children}
+    </MainContext.Provider>
+  );
+};
+
+export default MainContextProvider;
