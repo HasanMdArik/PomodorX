1196f6f4fd053389c6c0da8a27cb388622142b21
Mohammad <mohammadarik63@gmail.com>
2022-07-06 06:37:50 +0600

    main context interface added

--- src/data/dataTypes.ts
+++ src/data/dataTypes.ts
@@ -1,3 +1,13 @@
 type timeStep = "work" | "long" | "short";
 
-export { timeStep };
+type timeStepsData = {
+  pastLoopCount: number;
+  stepsArray: Array<timeStep>;
+};
+
+interface mainContextInterface {
+  stepsData: timeStepsData;
+  setStepsData: React.Dispatch<React.SetStateAction<timeStepsData>>;
+}
+
+export { timeStep, timeStepsData, mainContextInterface };
