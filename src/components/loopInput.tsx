e43477376119b1186a57d58e33a217b7cc848170
Mohammad <mohammadarik63@gmail.com>
2022-07-06 11:04:55 +0600

    Unused import removed

--- src/components/loopInput.tsx
+++ src/components/loopInput.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useMainContext } from "../contexts/mainContext";
-import { timeStepsData, timeStep } from "../data/dataTypes";
 
 const LoopInput = ({
   setState,
