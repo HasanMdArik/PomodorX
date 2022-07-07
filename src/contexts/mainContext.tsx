e9e905dda59c5c89f3ad8e80caf0dd989a680e16
Mohammad <mohammadarik63@gmail.com>
2022-07-07 07:03:16 +0600

    primary things added to the main context

--- src/contexts/mainContext.tsx
+++ src/contexts/mainContext.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode, useContext, useEffect, useState } from "react";
-import { mainContextInterface, loopData } from "../data/dataTypes";
+import {
+  mainContextInterface,
+  loopData,
+  timeStepData,
+} from "../data/dataTypes";
 
 const MainContext = React.createContext({} as mainContextInterface);
 
@@ -7,14 +11,55 @@ export const useMainContext = () => {
   return useContext(MainContext);
 };
 
-const MainContextProvider = ({
-  children,
-  setState,
-}: {
-  children: ReactNode;
-  setState: React.Dispatch<React.SetStateAction<number>>;
-}) => {
-  return <>{children}</>;
+const MainContextProvider = ({ children }: { children: ReactNode }) => {
+  //? The state variable defines the state of the app
+  // 0 stands for timer not started
+  // 1 stands for work time
+  // 2 stands for short break
+  // 3 stands for long break
+  const [state, setState] = useState(0);
+  //? The loopData will be used to get loop-count from loopInput.tsx
+  const [loopData, setLoopData] = useState<loopData>({
+    loopCount: 0,
+    pastLoopCount: 0,
+  });
+  //? The running step indicates the index of currently running step
+  const [runningStep, setRunningStep] = useState(-1);
+
+  //? The timeSteps state will be used by timeStepMenu.tsx and countdown.tsx
+  const [timeSteps, setTimeSteps] = useState<Array<timeStepData>>([]);
+
+  //* The UseEffect Functions to look for updates
+
+  //* The other functions
+  //? The function to cancel the timer
+  const cancelTimer = () => {};
+
+  //? The function to pause the timer
+  const pauseTimer = () => {};
+
+  //? The function to resume the timer
+  const resumeTimer = () => {};
+
+  //? The function to start the next step from the steps
+  const startNextStep = () => {};
+
+  const contextValues: mainContextInterface = {
+    loopData,
+    runningStep,
+    state,
+    timeSteps,
+    setLoopData,
+    cancelTimer,
+    pauseTimer,
+    resumeTimer,
+    startNextStep,
+  };
+  return (
+    <MainContext.Provider value={contextValues}>
+      {children}
+    </MainContext.Provider>
+  );
 };
 
 export default MainContextProvider;
