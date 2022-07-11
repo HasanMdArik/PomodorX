03c786aa1e671d55aa3fa7aeedd0ac62a5fbb714
Mohammad <mohammadarik63@gmail.com>
2022-07-11 20:08:08 +0600

    App added to the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,31 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
+import App from "../App/App";
+import MainContextProvider from "../contexts/mainContext";
+import "../styles/global.css";
 
-const NotFoundPage = () => {
+const PageNotFound = () => {
   return (
-    <main>
-      <h1>Hi</h1>
-    </main>
+    <React.Fragment>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Proza+Libre:wght@500;600;800&display=swap"
+          rel="stylesheet"
+        />
+        <title>PomodorX</title>
+      </Helmet>
+
+      <MainContextProvider>
+        <App />
+      </MainContextProvider>
+    </React.Fragment>
   );
 };
 
-export default NotFoundPage;
+export default PageNotFound;
